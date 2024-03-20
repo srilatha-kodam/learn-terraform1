ff772be33d56f8e0ecd0a0508b0f234f542857f5
Srilatha <srilatha.ssg@email.com>
2024-03-20 15:39:02 -0400

    resource changes

--- resource.tf
+++ resource.tf
@@ -1,3 +1,5 @@
-resouresource "aws_instance" "web" {
-  ami           = "ami-05f020f5935e52dc4 "
+
+resource "aws_instance" "web" {
+  ami           = "ami-05f020f5935e52dc4"
+  instance_type = "t2.micro"
 }
