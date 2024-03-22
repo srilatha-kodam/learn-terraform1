c91efaf873c3d295b63bce9fbbbe6fedf39753c8
Srilatha <srilatha.ssg@email.com>
2024-03-22 17:20:59 -0400

    tags changes

--- test.tf
+++ test.tf
@@ -1,7 +1,9 @@
 resource "aws_instance" "frontend" {
     ami       = "ami-05f020f5935e52dc4"
 instance_type = "t2.micro" 
-tags = "frontend"
+tags= {
+    name="frontend"
+}
 vpc_security_group_ids = "vpc-09edb7deaeb81e572" 
   
 }
@@ -10,14 +12,18 @@ vpc_security_group_ids = "vpc-09edb7deaeb81e572"
 resource "aws_instance" "backend" {
     ami       = "ami-05f020f5935e52dc4"
 instance_type = "t2.micro"
-tags = "backend"
-vpc_security_group_ids = "vpc-09edb7deaeb81e572" 
+tags={
+    name = "backend"
+}
+vpc_security_group_ids = [vpc-09edb7deaeb81e572] 
   
 }
 resource "aws_instance" "mysql" {
     ami       = "ami-05f020f5935e52dc4"
 instance_type = "t2.micro" 
-tags = "mysql"
-vpc_security_group_ids = "vpc-09edb7deaeb81e572 "
+tags = {
+    name="mysql"
+}
+vpc_security_group_ids = [vpc-09edb7deaeb81e572 ]
   
 }
