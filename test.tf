f42f4bc12000fd9b16aeeb6d7c8030790d82a61a
Srilatha <srilatha.ssg@email.com>
2024-03-22 16:42:32 -0400

    instance creations

--- test.tf
+++ test.tf
@@ -1,3 +1,23 @@
-output "test"{
-value="hello world"
+resource "aws_instance" "frontend" {
+    ami       = "ami-05f020f5935e52dc4"
+instance_type = "t2.micro" 
+tags = "frontend"
+vpc_security_group_ids = "vpc-09edb7deaeb81e572" 
+  
+}
+
+
+resource "aws_instance" "backend" {
+    ami       = "ami-05f020f5935e52dc4"
+instance_type = "t2.micro"
+tags = "backend"
+vpc_security_group_ids = "vpc-09edb7deaeb81e572" 
+  
+}
+resource "aws_instance" "mysql" {
+    ami       = "ami-05f020f5935e52dc4"
+instance_type = "t2.micro" 
+tags = "mysql"
+vpc_security_group_ids = "vpc-09edb7deaeb81e572 "
+  
 }
